2e1502628d76990ea5f7732c7ca2af9117780dd6
nathazz <nathansg907@gmail.com>
2025-05-16 14:06:03 -0300

    chore: add port for build

--- main.go
+++ main.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 	"scraper-go/src/middleware"
 	"scraper-go/src/routes"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+
 	r := gin.Default()
 	r.Use(middleware.RateLimitByIP())
 
 	routes.SetupRouter(r)
 
-	err := r.Run(":8080")
-
+	err = r.Run(":" + port)
 	if err != nil {
-		return
+		log.Fatal("Error to init server:", err)
 	}
 }
